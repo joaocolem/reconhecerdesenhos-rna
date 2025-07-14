fa92913c8be6b02989f357044b81c2cb7179371e
Joao Colem <joaocostaval@gmail.com>
2025-07-14 10:57:30 -0300

    feat: InitialProject

---
+++ Bolinha.java
@@ -0,0 +1,27 @@
+package reconhecerdesenhos;
+
+import java.awt.Color;
+import java.util.Random;
+
+public class Bolinha {
+    String nome;
+    Color cor;
+    int x = 0;
+    int y = 0;
+    int cont = 0;
+    int contColisoes = 0;
+    int velocidade = 0;
+    RedeNeural redeNeural;
+    boolean acertou = false;
+    int pontos = 0;
+    int pontosGeral = 0;
+
+    public Bolinha(String nome, Color cor, int x, int y) {
+        this.nome = nome;
+        this.cor = cor;
+        this.x = x;
+        this.y = y;
+        Random random = new Random();
+        this.velocidade = random.nextInt(1, 6);
+    }
+}
